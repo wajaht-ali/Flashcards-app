70ccfe2fcaa842fe096665cdf5cc79a1155f7f92
wajaht-ali <contact.chwajahat@gmail.com>
2024-06-10 13:42:54 +0500

    Admin Dashboard development comppleted

--- client/src/components/pages/Admin/Dashboard.jsx
+++ client/src/components/pages/Admin/Dashboard.jsx
@@ -4,6 +4,11 @@ import Layout from '../../Layout/Layout'
 import { useAuth } from '../../../context/auth'
 import { FaBarsStaggered } from "react-icons/fa6";
 import Sidebar from './Sidebar';
+import { FaPlus } from "react-icons/fa6";
+import { CgFileAdd } from "react-icons/cg";
+import { FiUsers } from "react-icons/fi";
+import { ImStatsBars } from "react-icons/im";
+import { Link } from 'react-router-dom';
 
 const Dashboard = () => {
   const [auth, setAuth] = useAuth();
@@ -19,10 +24,39 @@ const Dashboard = () => {
         <div className={`sidebar ${sidebar ? 'active' : ''}`}>
           <Sidebar />
         </div>
-        <div className="w-full shadow-sm shadow-gray-200 h-screen overflow-y-auto p-4">
+        <div className="w-full shadow-sm shadow-gray-200 h-screen overflow-y-auto p-4 flex flex-col items-start">
           <div>
-            <h1>Welcome back, <span className="text-blue-700 font-semibold text-lg">Admin!</span></h1>
+            <h1 className='font-semibold text-lg md:text-3xl'>Welcome back, <span className="text-blue-700">Admin!</span></h1>
+            <p>What&apos;s your plan for today?</p>
           </div>
+
+          <div className="w-full flex flex-wrap items-center justify-around mt-20">
+            <Link to={"/dashboard/admin/create-card"}>
+              <div className="flex flex-col items-center justify-center shadow-lg h-[200px] w-[300px] rounded-lg p-4 transition-all hover:scale-90 cursor-pointer hover:shadow-lg my-2 md:my-0">
+                <div className="w-auto table items-center p-4 bg-gray-200 rounded-full hover:bg-gray-300"><FaPlus size={25} /></div>
+                <p className="mt-10 text-lg">Create Card</p>
+              </div>
+            </Link>
+            <Link to={"/dashboard/admin/create-card"}>
+              <div className="flex flex-col items-center justify-center shadow-lg h-[200px] w-[300px] rounded-lg p-4 transition-all hover:scale-90 cursor-pointer hover:shadow-lg my-2 md:my-0">
+                <div className="w-auto table items-center p-4 bg-gray-200 rounded-full hover:bg-gray-300"><CgFileAdd size={25} /></div>
+                <p className="mt-10 text-lg">Create Subject</p>
+              </div>
+            </Link>
+            <Link to={"/dashboard/admin/create-card"}>
+              <div className="flex flex-col items-center justify-center shadow-lg h-[200px] w-[300px] rounded-lg p-4 transition-all hover:scale-90 cursor-pointer hover:shadow-lg my-2 md:my-0">
+                <div className="w-auto table items-center p-4 bg-gray-200 rounded-full hover:bg-gray-300"><FiUsers size={25} /></div>
+                <p className="mt-10 text-lg">All Users</p>
+              </div>
+            </Link>
+            <Link to={"/dashboard/admin/create-card"}>
+              <div className="flex flex-col items-center justify-center shadow-lg h-[200px] w-[300px] rounded-lg p-4 transition-all hover:scale-90 cursor-pointer hover:shadow-lg my-2 md:my-0">
+                <div className="w-auto table items-center p-4 bg-gray-200 rounded-full hover:bg-gray-300"><ImStatsBars size={25} /></div>
+                <p className="mt-10 text-lg">Statistics</p>
+              </div>
+            </Link>
+          </div>
+
         </div>
       </div>
     </Layout>
