ae2b3ef1caa20c968002a771f239b877a0d760e9
wajaht-ali <contact.chwajahat@gmail.com>
2024-06-13 13:32:06 +0500

    Create Card component completed

--- client/src/components/pages/Admin/Cards.jsx
+++ client/src/components/pages/Admin/Cards.jsx
@@ -34,8 +34,30 @@ const Cards = () => {
                     <div className="max-w-2xl mx-auto">
                         <h2 className="font-semibold text-xl md:text-2xl text-center my-3 text-indigo-700">Create a Card</h2>
                         <form onSubmit={handleFormSubmit}>
+                            <div className="px-3 py-2 my-2">
 
-                        <button onClick={handleFormSubmit}>Submit</button>
+                                <label htmlFor="helper-text" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Topic Title</label>
+                                <input type="text" id="helper-text" aria-describedby="helper-text-explanation" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Title of topic..." />
+                            </div>
+                            <div className="px-3 py-2 my-2">
+
+                                <label htmlFor="countries" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Select Category</label>
+                                <select id="countries" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                    <option selected>Choose Category</option>
+                                    <option value="ONE">Subject One</option>
+                                    <option value="TWO">Subject Two</option>
+                                    <option value="THR">Subject Three</option>
+                                    <option value="FOU">Subject Four</option>
+                                </select>
+
+                            </div>
+                            <div className="px-3 py-2 my-2">
+
+                                <label htmlFor="message" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Topic Notes</label>
+                                <textarea id="message" rows="10" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here..."></textarea>
+
+                            </div>
+                            <button type="button" className=" mx-3 text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 ">Submit</button>
                         </form>
                     </div>
                 </div>
