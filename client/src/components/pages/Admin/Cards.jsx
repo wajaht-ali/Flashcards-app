4417f47a144a1797110f2b0eb0b69df71df23e71
wajaht-ali <contact.chwajahat@gmail.com>
2024-07-07 09:56:35 +0500

    Create card text limitation added

--- client/src/components/pages/Admin/Cards.jsx
+++ client/src/components/pages/Admin/Cards.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-vars */
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Layout from '../../Layout/Layout'
 import { useAuth } from '../../../context/auth'
 import { FaBarsStaggered } from "react-icons/fa6";
@@ -19,12 +19,11 @@ const Cards = () => {
         setSidebar(!sidebar);
     }
     const creator = auth?.user?.id;
-    console.log(creator);
     const handleFormSubmit = async (e) => {
         try {
             e.preventDefault();
-            const res = await axios.post("/api/v1/cards/create-card", {title, subject, status, content, creator});
-            if(res.data.success) {
+            const res = await axios.post("/api/v1/cards/create-card", { title, subject, status, content, creator });
+            if (res.data.success) {
                 setTitle("");
                 setSubject("");
                 setStatus("");
@@ -38,6 +37,47 @@ const Cards = () => {
             console.log(`Error with form submission ${error}`);
         }
     }
+    // input text validation
+    const myText = useRef(null);
+    const charResult = useRef(null);
+
+    const myTextArea = useRef(null);
+    const charTextResult = useRef(null);
+    const varLimit = 60;
+    const textAreaLimit = 1000;
+    useEffect(() => {
+        charResult.current.textContent = 0 + "/" + varLimit;
+        charTextResult.current.textContent = 0 + "/" + textAreaLimit;
+        if (myText && charResult) {
+            myText.current.addEventListener("input", () => {
+                const textLength = myText.current.value.length;
+                charResult.current.textContent = textLength + "/" + varLimit;
+                if (textLength == varLimit) {
+                    charResult.current.style.color = "#ff2851";
+                    myText.current.style.borderColor = "#ff2851";
+                }
+                else {
+                    charResult.current.style.color = "#333";
+                    myText.current.style.borderColor = "#3b82f6";
+                }
+            })
+        }
+        if (myTextArea && charTextResult) {
+            myTextArea.current.addEventListener("input", () => {
+                const textAreaLength = myTextArea.current.value.length;
+                charTextResult.current.textContent = textAreaLength + "/" + textAreaLimit;
+                if (textAreaLength == textAreaLimit) {
+                    charTextResult.current.style.color = "#ff2851";
+                    myTextArea.current.style.borderColor = "#ff2851";
+                }
+                else {
+                    charTextResult.current.style.color = "#333";
+                    myTextArea.current.style.borderColor = "#3b82f6";
+                }
+            })
+        }
+
+    }, [])
     return (
         <Layout>
             <div className="w-full flex flex-row items-start justify-between gap-2">
@@ -55,8 +95,12 @@ const Cards = () => {
 
                                 <label htmlFor="helper-text" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Topic Title</label>
                                 <input type="text" id="helper-text" aria-describedby="helper-text-explanation"
-                                    className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Title of topic..."
-                                    onChange={(e) => setTitle(e.target.value)} />
+                                    className="input-text bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Title of topic..."
+                                    onChange={(e) => setTitle(e.target.value)}
+                                    maxLength={60}
+                                    ref={myText} />
+                                <p className="result text-end my-2 text-[14px] font-light"
+                                    ref={charResult} ></p>
                             </div>
                             <div className="px-3 py-2 my-2">
 
@@ -115,15 +159,18 @@ const Cards = () => {
                                 <textarea id="message" rows="10"
                                     className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                                     placeholder="Write your thoughts here..."
-                                    onChange={(e) => setContent(e.target.value)}></textarea>
-
+                                    onChange={(e) => setContent(e.target.value)}
+                                    ref={myTextArea}
+                                    maxLength={1000}></textarea>
+                                <p className="text-limit-info text-end my-2 text-[14px] font-light"
+                                    ref={charTextResult}></p>
                             </div>
                             <button onClick={handleFormSubmit} type="button" className=" mx-3 text-white bg-gradient-to-r from-blue-500 via-blue-600 to-blue-700 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 shadow-lg shadow-blue-500/50 dark:shadow-lg dark:shadow-blue-800/80 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 ">Submit</button>
                         </form>
                     </div>
                 </div>
-            </div>
-        </Layout>
+            </div >
+        </Layout >
     )
 }
 
